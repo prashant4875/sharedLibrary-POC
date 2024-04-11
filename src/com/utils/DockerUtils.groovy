96a18e1848b4ff096ee0766c678b3e8587eb8eaa
Prashant <bhadoria998@gmail.com>
2024-04-11 17:20:45 +0530

    Add

--- src/com/utils/DockerUtils.groovy
+++ src/com/utils/DockerUtils.groovy
@@ -12,6 +12,7 @@ class DockerUtils {
             steps.sh "docker build -t devopsweb ."
             steps.sh "docker tag devopsweb prashant4875/devopsweb:latest"
             steps.sh "docker push prashant4875/devopsweb:latest"
+            steps.sh "docker run -d --name devopsweb -p 80:8080 prashant4875/devopsweb:latest"
         }
     }
 }
